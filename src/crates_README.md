4889912d01c43ddf6d6eba96ef001be7d5247d7e
Arthur Beck <averse.abfun@gmail.com>
2024-12-25 18:02:23 -0600

    Added some documentation

--- src/crates_README.md
+++ src/crates_README.md
@@ -8,3 +8,5 @@ replace-with = 'local-registry'
 [source.local-registry]
 local-registry = '{?TOOLCHAIN.CRATES_DIR}' # double-check this path is correct, especially if you are creating the package on a different machine than the machine you are creating the package for
 ```
+
+(psst- if you know how to install crate files to the registry manually, make an issue or PR on [rust-pkg-gen](https://github.com/AverseABFun/rust-pkg-gen)!)
--- src/template/build.sh
+++ src/template/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-CI=1
-cd "$(dirname "$0")"
+# shellcheck shell=bash
+cd "$(dirname "$0")" || exit
 cargo generate-lockfile --verbose
-"$CARGO_BIN_FILE_CARGO_LOCAL_REGISTRY" local-registry --sync Cargo.lock crates > /dev/null 2>&1
-    # ^^^ is super jank, but it expects that the first argument is the provided subcommand of cargo.
+"$CARGO_BIN_FILE_CARGO_LOCAL_REGISTRY" local-registry --sync Cargo.lock crates >/dev/null 2>&1
+# ^^^ is super jank, but it expects that the first argument is local-registry and acts strangely otherwise
 
 mv crates/* .
 if [[ "$1" != "save" ]]; then
